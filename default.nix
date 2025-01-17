d394c5f97a64530f98f8b80e941e530cb5b0f001
Sonya Sireneva <sleirsgoevy@gmail.com>
2025-01-17 20:23:38 +0300

    flake: make package naming match that of tdesktop

--- default.nix
+++ default.nix
@@ -1,8 +1,10 @@
 { lib, tdesktop }:
 
 tdesktop.override {
+  pname = "yukigram";
   unwrapped = tdesktop.unwrapped.overrideAttrs (f: {
-    name = "yukigram";
+    name = "yukigram-unwrapped";
+    version = builtins.elemAt (builtins.match ".*(^|\n)AppVersionStrSmall *([^ ]*)($|\n).*" (builtins.readFile Telegram/build/version)) 1;
     src = ./.;
   });
 }
